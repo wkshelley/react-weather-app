9ca2f1c99a892e59f044256782ab46c017085175
wkshelley <whiteshelley14@yahoo.com>
2021-10-04 11:14:42 -0400

    Added Git Hub Repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This Project was created by Shelley White-Lyons and is open-sourced
+        <a
+          href="https://github.com/wkshelley/react-weather-application"
+          target="_blank" rel="noreferrer"
+        >
+          {" "}
+          on Git Hub
+        </a>
+      </footer>
+    </div>
+  );
 }
