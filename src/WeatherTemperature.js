18255229995f74ed7556d50154579414becc38b6
wkshelley <whiteshelley14@yahoo.com>
2021-10-27 00:07:25 -0400

    completed temperature conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -5,18 +5,19 @@ export default function WeatherTemperature(props) {
 
   function showCelsius(event) {
     event.preventDefault();
-    return ((props.farenheit - 32) * 5) / 9;
 
-    //setUnits("celsius");
+    setUnits("celsius");
   }
-  function showFarenheit() {
-    //event.preventDefault();
+  function showFarenheit(event) {
+    event.preventDefault();
     setUnits("farenheit");
   }
-
+  function celsius() {
+    return ((props.farenheit - 32) * 5) / 9;
+  }
   if (units === "farenheit") {
     return (
-      <div className={WeatherTemperature}>
+      <div className="WeatherTemperature">
         <div className="row">
           <div className="col-6">
             <span className="d-flex">
@@ -26,12 +27,16 @@ export default function WeatherTemperature(props) {
               <a
                 href="/"
                 className=" temperature justify-content-around "
-                onClick={showFarenheit}
+                //  onClick={showFarenheit}
               >
                 F{" "}
               </a>
               <div className="temperature">︱</div>
-              <a href="/" className=" temperature justify-content-around ">
+              <a
+                href="/"
+                className=" temperature justify-content-around "
+                onClick={showCelsius}
+              >
                 C
               </a>
             </span>
@@ -42,22 +47,33 @@ export default function WeatherTemperature(props) {
   } else {
     //still working on F to C conversion.   showCelsius is not being called on the return
     return (
-      <div>
-        {showCelsius()}
-        <a
-          href="/"
-          className=" temperature justify-content-around "
-          // onClick={showCelsius}
-        >
-          C
-        </a>
-        <a
-          href="/"
-          className=" temperature justify-content-around "
-          //onClick={showFarenheit}
-        >
-          F{" "}
-        </a>
+      <div className="WeatherTemperature">
+        <div className="row">
+          <div className="col-6">
+            <span className="d-flex">
+              <h2 className="temperature units float-left">
+                {" "}
+                {Math.round(celsius())}˚
+              </h2>
+
+              <a
+                href="/"
+                className=" temperature justify-content-around "
+                onClick={showCelsius}
+              >
+                C
+              </a>
+              <div className="temperature">︱</div>
+              <a
+                href="/"
+                className=" temperature justify-content-around "
+                onClick={showFarenheit}
+              >
+                F{" "}
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
