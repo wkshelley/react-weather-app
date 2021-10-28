ae98f0e3b6072f20c7500a708b2cd8a639eee82b
wkshelley <whiteshelley14@yahoo.com>
2021-10-28 12:33:54 -0400

    added WeatherForecast API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,13 +1,22 @@
 import React from "react";
+import axios from "axios";
 import "./WeatherForecast.css";
 import WeatherImage from "./WeatherImage";
 
 export default function WeatherForecast() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "969aa20a54046a1f43968e313b89d478";
+  let longitude = 39.9612;
+  let latitude = 82.9988;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          <div className="WeatherForecat-day">Tues {""}</div>
+          <div className="WeatherForecat-day">Tues</div>
           <div className="WeatherForecast-icon"></div>
           <WeatherImage code="01d" size={30} />
           <div className="WeatherForecast-temperatures">
