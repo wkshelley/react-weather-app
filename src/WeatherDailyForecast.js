c3d2187f722ee444205466a0b0c01d00ec315b01
wkshelley <whiteshelley14@yahoo.com>
2021-10-31 10:52:18 -0400

    Update WeatherDailyForecast.js

--- src/WeatherDailyForecast.js
+++ src/WeatherDailyForecast.js
@@ -6,16 +6,14 @@ export default function WeatherDailyForecast(props) {
     <div>
       <div className="WeatherForecat-day">{day()}</div>
       <div className="WeatherForecast-icon">
-        <WeatherImage code={props.data[0].image} size={30} />
+        <WeatherImage code={props.data.weather[0].icon} size={30} />
       </div>
       <div className="WeatherForecast-temperatures">
-        <span className="WeatherForecast-temp-min">
-          {props.data[0].data.temp.min}˚
-        </span>
+        <span className="WeatherForecast-temp-min">{props.data.temp.min}˚</span>
 
         <span className="WeatherForecast-temp-max">
           {" "}
-          {props.data[0].data.temp.max}˚
+          {props.data.temp.max}˚
         </span>
       </div>
     </div>
